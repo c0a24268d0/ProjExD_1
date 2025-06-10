b0714075ab227aad679d8fc8790e46c822bfb229
yuzuki <c0a24268d0@edu.teu.ac.jp>
2025-06-10 15:36:07 +0900

    右から左

--- pygame_image.py
+++ pygame_image.py
@@ -11,13 +11,13 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像のsurface
     kk_img = pg.image.load("fig/3.png")     #こうかとんのsurface
-    kk_img=pg.transform.flip(kk_img,True,False)
+    kk_img=pg.transform.flip(kk_img,True,False) #こうかとんの左右反転
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-tmr, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
